8d86f75b963b3a85638682c8c3cc1bf732fb21e4
tianshi <tianshi@chuangjia.me>
2017-02-08 23:15:56 +0800

    add mocha test

---
+++ test/server/testServer.spec.js
@@ -0,0 +1,9 @@
+import should from 'should';
+
+describe('Array', function() {
+    describe('indexOf', function() {
+        it('should return true 11', function() {
+            [1,2,3].indexOf(1).should.equal(0);
+        })
+    })
+})
